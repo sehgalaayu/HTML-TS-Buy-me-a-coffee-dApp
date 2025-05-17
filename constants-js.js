81bd1557edab8b2a52ae5b5d336acc42f56c426f
sehgalaayu <119155282+sehgalaayu@users.noreply.github.com>
2025-05-17 20:34:14 +0530

    Update constants-js.js

--- constants-js.js
+++ constants-js.js
@@ -1,6 +1,6 @@
 // constants.js
 
-export const contractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+export const contractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F052"
 export const coffeeAbi = [
   {
     inputs: [{ internalType: "address", name: "priceFeed", type: "address" }],
